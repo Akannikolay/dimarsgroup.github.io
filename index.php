c704142e2508af4a9670356c8cb82ffcd8daf316
Akannikolay <akan.nikolay@yandex.ru>
2020-10-11 15:43:30 +0300

    del

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-preg_match('/^\w{2}/', $_SERVER['HTTP_ACCEPT_LANGUAGE'], $m);
-switch (strtolower($m[0])) {
-case 'ru': header('Location: /ru/'); break;
-default: header('Location: /en/'); break;
-}
-?>
